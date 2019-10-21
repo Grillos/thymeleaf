24791de162cd903d388b43b8e930a19a7eb57ddc
Robson Silva Grillo <robson.grillo@hdi.com.br>
2019-10-21 15:35:58 -0300

    feat(fragment menu): cria fragmento de menu

--- src/main/java/com/thymeleaf/controller/UserController.java
+++ src/main/java/com/thymeleaf/controller/UserController.java
@@ -22,6 +22,13 @@ public class UserController {
         return "users";
     }
     
+    @GetMapping("/users-list")
+    public String list(Model model) {
+    	model.addAttribute("users", userService.findAll());
+    	model.addAttribute("user", new User());
+        return "list-users";
+    }
+    
     @PostMapping("/users")
     public String save(@ModelAttribute User user, Model model) {
     	userService.save(user);
